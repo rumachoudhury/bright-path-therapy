c7e9b3ca7aa4b2f3b8c769810fe4385721ec8d0e
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-15 01:04:53 -0400

    Setup Bright Path Therapy frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
+
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
 
 app.get("/", (req, res) => {
   res.json({
