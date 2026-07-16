51020fdc27468861a865858c65644267589a9dc5
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-16 01:09:32 -0400

    Created contactController.js

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,4 +1,21 @@
 const Contact = require("../models/Contact");
 
+const createContact = async (req, res) => {
+  try {
+    const newContact = await Contact.create(req.body);
 
-const createContact = 
+    res.status(201).json({
+      message: "Contact request received",
+      data: newContact,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  createContact,
+};
