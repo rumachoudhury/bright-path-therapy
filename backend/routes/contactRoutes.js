f4a1659232d4030954d34feffa66d2599612ea0d
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-16 23:35:51 -0400

    Updated the contactRoute

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const { createContact } = require("../controllers/contactController");
 
 router.post("/", createContact);
+router.get("/api/contact");
+router.put("/api/contact/:id");
+router.delete("/api/contact/:id");
 
 module.exports = router;
