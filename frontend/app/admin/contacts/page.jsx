d29a0753f835bfbffdd1a84b6028b02c4120e9d6
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-17 22:27:32 -0400

    Updated AdminContacts Page

--- frontend/app/admin/contacts/page.jsx
+++ frontend/app/admin/contacts/page.jsx
@@ -1,7 +1,47 @@
+"use cliect"
+
+import { useEffect, useState,  } from "react";
+
+
+
 export default function AdminContacts() {
+
+    const [contacts setContacts] = useState([])
+   
+
+    useEffect (() => {
+        fetch("http://localhost:5000/api/contact")
+        .then((res) => res.json())
+        .then((data) => setContacts(data.data))
+        .catch((err) => console.log(err))
+    }, [])
+
+
   return (
-    <main>
-      <h1>Contact Messages</h1>
+    <main className="p-10">
+      <h1 className="text-3xl font-bold mb-6">Contact Messages</h1>
+
+      <table className="w-full border border-gray-300">
+        <thead>
+          <tr className="bg-gray-100">
+            <th className="border p-3">Name</th>
+            <th className="border p-3">Email</th>
+            <th className="border p-3">Phone</th>
+            <th className="border p-3">Message</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {AdminContacts.map((contact) => (
+            <tr key={contact._id}>
+              <td className="border p-3"></td>
+              <td className="border p-3"></td>
+              <td className="border p-3"></td>
+              <td className="border p-3"></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </main>
   );
 }
