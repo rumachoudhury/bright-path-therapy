b193dc9a09b200f19cf19cdeec7d09afd6c30857
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-01 19:30:11 -0400

    Add table to the appointments page

--- frontend/app/admin/appointments/page.jsx
+++ frontend/app/admin/appointments/page.jsx
@@ -11,6 +11,31 @@ function AppointmentsPage() {
         <ArrowLeft />
         Bact to home
       </Link>
+
+      <h1 className="text-3xl font-bold mb-6">Appoinments</h1>
+
+      {appoinments.length === 0 ? (
+        <div>
+          <h2 className="text-gray-500">No appoinments found.</h2>
+
+          <p>Consultation required here.</p>
+        </div>
+      ) : (
+        <div className="overflow-x-auto border rounded-lg">
+          <table>
+            <thead>
+              <tr className="bg-gray-100">
+                <th className="border p-3 text-left">ID</th>
+                <th className="border p-3 text-left">Patient Name</th>
+                <th className="border p-3 text-left">Email</th>
+                <th className="border p-3 text-left">Date</th>
+                <th className="border p-3 text-left">Time</th>
+                <th className="border p-3 text-left">Status</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      )}
     </main>
   );
 }
