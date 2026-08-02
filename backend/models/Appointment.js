a3ee981c5b42cecf3a2d650d2b5399e20a101d7e
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-01 22:26:10 -0400

    Completed models/Appointment.js

---
+++ backend/models/Appointment.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const appoinmentSchema = new mongoose.Schema(
+  {
+    patientName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    phone: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    date: {
+      type: String,
+      required: true,
+    },
+
+    time: {
+      type: String,
+      required: true,
+    },
+
+    ststus: {
+      type: String,
+      enum: ["Pending", "Approved", "Cancelled"],
+      default: "Pending",
+    },
+
+    notes: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.export = mongoose.model("Appoinment", appoinmentSchema);
