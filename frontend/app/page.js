ecef19486e2fc37d2a314f84550526075e69c42c
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-06 09:38:40 -0400

    Add Leaflet map component

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,7 +3,8 @@ import Hero from "@/components/Hero";
 import Signs from "@/components/Signs";
 import About from "@/components/About";
 import Services from "@/components/Services";
-import Area from "@/components/Area";
+// import Area from "@/components/Area";
+import Map from "@/components/Map";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import { services } from "@/lib/services";
@@ -18,7 +19,8 @@ export default function Home() {
       <About />
       <Services services={services} />
       <FAQ />
-      <Area />
+      {/* <Area /> */}
+      <Map />
       <Contact />
       <Footer />
     </>
--- frontend/components/Map.jsx
+++ frontend/components/Map.jsx
@@ -1,15 +1,37 @@
 "use client";
 
 import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import L from "leaflet";
 
-<MapContainer
-  center={[40.7128, -74.006]}
-  zoom={13}
-  style={{ height: "400px", width: "100%" }}
->
-  <TileLayer
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    attribution="&copy; OpenStreetMap contributors"
-  />
-  <Marker position={[40.7128, -74.006]} />
-</MapContainer>;
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+// delete L.Icon.Default.prototype._getIconUrl;
+const defaultIcon = L.icon({
+  iconUrl: icon.src,
+  shadowUrl: iconShadow.src,
+});
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "/marker-icon-2x.png",
+  iconUrl: "/marker-icon.png",
+  shadowUrl: "/marker-shadow.png",
+});
+
+export default function Map() {
+  return (
+    <MapContainer
+      center={[40.7128, -74.006]}
+      zoom={13}
+      style={{ height: "400px", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+
+      <Marker position={[40.7128, -74.006]} />
+    </MapContainer>
+  );
+}
