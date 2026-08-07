842ee12b116bc9ec7c4de802deedc4638e0ce9da
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-07 11:25:50 -0400

    Fix next-themes defaultTheme config

--- frontend/components/ThemeProvider.jsx
+++ frontend/components/ThemeProvider.jsx
@@ -4,7 +4,7 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({ children }) {
   return (
-    <NextThemesProvider attribute="class" default="light">
+    <NextThemesProvider attribute="class" defaultTheme="light">
       {children}
     </NextThemesProvider>
   );
